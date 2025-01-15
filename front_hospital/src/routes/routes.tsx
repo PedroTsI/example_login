4593ebbc99771fad67b79b5688ab16453f3d7604
Pedro Paulo Cantalice <88915142+PedroTsI@users.noreply.github.com>
2025-01-15 18:18:17 -0300

    Delete front_hospital directory

--- front_hospital/src/routes/routes.tsx
+++
@@ -1,6 +0,0 @@
-import { RouterProvider, Routes } from "react-router-dom";
-import { userRoutes } from "./userRoutes";
-
-export const AppRoutes = (): JSX.Element => {
-  return <RouterProvider router={userRoutes} />;
-};
--- front_hospital/src/utils/date-formatter.ts
+++
@@ -1,8 +0,0 @@
-export const date_format = (date: string) => {
-    const parts = date.split("-");
-    if (parts.length === 3) {
-      const [year, month, day] = parts;
-      return `${day}/${month}/${year}`;
-    }
-    return date;
-};
